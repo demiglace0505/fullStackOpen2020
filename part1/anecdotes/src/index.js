e4d6478acc8710bf800e5832b805d689f9899233
demiglace0505 <60604762+demiglace0505@users.noreply.github.com>
2020-06-03 14:33:21 +0800

    finished part 1

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVote] = useState(new Array(anecdotes.length).fill(0));
+  const highestVoted = votes.indexOf(votes.reduce((a, b) => Math.max(a, b)))
+
+  const randomAnecdote = () => {
+    return () => {
+      setSelected(Math.floor(Math.random() * anecdotes.length))
+    }
+  }
+
+  const vote = () => {
+    return () => {
+      const voteHolder = [...votes]
+      voteHolder[selected] += 1
+      setVote(voteHolder)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>
+        {anecdotes[selected]} <br />
+        has {votes[selected]} votes
+      </div>
+      <div>
+        <button onClick={vote()}>Vote</button>
+        <button onClick={randomAnecdote()}>Next Anecdote</button>
+      </div>
+
+      <div>
+        <h1>Anecdote with most votes</h1>
+        {anecdotes[highestVoted]} <br />
+        has {votes[highestVoted]} votes
+      </div>
+
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = ({ text, handleClick }) => {
+    return (
+        <span>
+            <button onClick={handleClick}>{text}</button>
+        </span>
+    )
+}
+
+const Statistic = ({ text, value }) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
+const Statistics = (props) => {
+
+    if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <div>No feedback given</div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h1>statistics</h1>
+            <table>
+                <tbody>
+                    <Statistic text="good" value={props.good} />
+                    <Statistic text="neutral" value={props.neutral} />
+                    <Statistic text="bad" value={props.bad} />
+                    <Statistic text="all" value={props.total} />
+                    <Statistic text="average" value={props.average} />
+                    <Statistic text="positive" value={props.percent} />
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const total = good + neutral + bad;
+    const average = (good - bad) / total;
+    const percent = good / total * 100 + " %";
+
+    const handleGood = () => {
+        return () => {
+            setGood(good + 1)
+        }
+    }
+
+    const handleNeutral = () => {
+        return () => {
+            setNeutral(neutral + 1)
+        }
+    }
+
+    const handleBad = () => {
+        return () => {
+            setBad(bad + 1);
+        }
+    }
+
+    return (
+        <div>
+            <h1>give feedback</h1>
+
+            <Button handleClick={handleGood()} text="good" />
+            <Button handleClick={handleNeutral()} text="neutral" />
+            <Button handleClick={handleBad()} text="bad" />
+
+            <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} percent={percent} />
+
+        </div>
+    )
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
