b4b7a32d7623f6affbbf3d39123de3a3c36df98a
demiglace0505 <60604762+demiglace0505@users.noreply.github.com>
2020-09-21 19:04:13 +0800

    7.18, prep for theming

---
+++ Reference/7.18/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  Switch, Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return (
+        <h1>
+          Serialize yourself...
+        </h1>
+  )
+}
+export default Home
