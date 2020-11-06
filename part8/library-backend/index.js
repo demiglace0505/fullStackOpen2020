4a10f95654e8775392bc3b2f57b43129f37e153c
demiglace0505 <demiglace0505@gmail.com>
2020-11-06 17:16:42 +0800

    8.16

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 const { ApolloServer } = require('apollo-server')
 const mongoose = require('mongoose')
+
+const User = require('./models/user.js')
 
 const typeDefs = require('./gql/typeDefs.js')
 const resolvers = require('./gql/resolvers.js')
 
 const MONGODB_URI = process.env.URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 console.log('connecting to', MONGODB_URI)
 
@@ -26,6 +29,19 @@ mongoose.connect(MONGODB_URI, {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    // console.log(req)
+    // console.log(auth)
+    // console.log(JWT_SECRET)
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
